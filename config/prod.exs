76a6418f2d953a623d042991c493848ead5f9c5f
Daniel Perez <daniel@claudetech.com>
2016-04-17 20:50:08 +0900

    Fix prod.exs.

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ config :buildy_push, BuildyPush.Endpoint,
 
 config :buildy_push, BuildyPush.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL") || raise("please provide DATABASE_URL")
+  url: System.get_env("DATABASE_URL") || raise("please provide DATABASE_URL"),
   pool_size: 20
 
 config :logger,
