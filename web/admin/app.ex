b956f05acd5713af578fea7ccf9f7dff52f994c1
Daniel Perez <tuvistavie@gmail.com>
2017-06-16 00:11:34 +0900

    Improve app and message creation

--- web/admin/app.ex
+++ web/admin/app.ex
@@ -1,21 +1,61 @@
 defmodule Hermex.ExAdmin.App do
   use ExAdmin.Register
 
+  import Phoenix.HTML.Format, only: [text_to_html: 1]
+
   register_resource Hermex.App do
     index do
       selectable_column()
 
       column :id
       column :name
+      column :platform
+      column :inserted_at
 
       actions()
+    end
+
+    show _app do
+      attributes_table do
+        row :id
+        row :platform
+        row :name
+        row :apns_certificate, &(raw text_to_html(Map.get(&1.settings, "cert") || ""))
+        row :apns_key, &(raw text_to_html(Map.get(&1.settings, "key") || ""))
+        row :gcm_auth_key, &(raw text_to_html(Map.get(&1.settings, "auth_key", "")))
+        row :inserted_at
+      end
     end
 
     form app do
       inputs do
         input app, :name
         input app, :platform, collection: ~w(apns gcm)
-        input app, :settings, schema: [auth_key: :string, cert: :string, key: :string]
+        input app, "GCM_auth_key", type: :string, name: "app[settings][auth_key]", value: app.settings["auth_key"]
+        input app, "APNS_certificate", type: :text, name: "app[settings][cert]", value: app.settings["cert"]
+        input app, "APNS_key", type: :text, name: "app[settings][key]", value: app.settings["key"]
+      end
+
+      javascript do
+        """
+        $(document).ready(function() {
+          var inputs = ['#app_GCM_auth_key_input', '#app_APNS_certificate_input', '#app_APNS_key_input'];
+          var updateInputs = function (currentPlatform) {
+            $.each(inputs, function (i, selector) {
+              if (!currentPlatform || (currentPlatform === 'gcm' && i !== 0) || (currentPlatform === 'apns' && i === 0)) {
+                $(selector).hide();
+              } else {
+                $(selector).show();
+              }
+            });
+          };
+          var platformInput = $('#app_platform_id');
+          $('#app_platform_id').change(function() {
+            updateInputs(this.value);
+          });
+          updateInputs(platformInput.val());
+        });
+        """
       end
     end
   end
--- web/admin/message.ex
+++ web/admin/message.ex
@@ -14,7 +14,39 @@ defmodule Hermex.ExAdmin.Message do
 
     form message do
       inputs do
-        input message, :data, type: :text
+        content do
+          content_tag(:div, class: "form-group") do
+            [content_tag(:label, "Data", class: "col-sm-2 control-label"),
+            content_tag(:div, class: "col-sm-10") do
+              content_tag(:div, "", id: "message_data")
+            end]
+          end
+        end
+        # input message, :raw_data, type: :text, name: "message[data]", label: "data", value: Poison.encode!(Map.get(message, :data) || %{}, pretty: true)
+        input message, :raw_options, type: :text, name: "message[options]", label: "options", value: Poison.encode!(Map.get(message, :options), pretty: true)
+        input message, :sender_key
+        input message, :scheduled_at, type: Ecto.DateTime
+
+        content do
+          content_tag(:script, "", src: "https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/5.7.0/jsoneditor.min.js")
+        end
+        content do
+          Phoenix.HTML.Tag.tag(:link, rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/5.7.0/jsoneditor.min.css")
+        end
+      end
+
+      javascript do
+        """
+        $(function() {
+          var messageTextarea = document.querySelector('#message_data');
+          var options = {
+            "mode": "text",
+            "indentation": 2
+          };
+          var editor = new JSONEditor(messageTextarea, options);
+          editor.set(#{Poison.encode!(message.data || %{})});
+        })
+        """
       end
     end
   end
